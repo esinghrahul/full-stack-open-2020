7780df5a9179e3cf7e9cde3290d79f31b6e019e4
esinghrahul <e.singh.rahul@outlook.com>
2020-08-10 19:27:50 +0530

    Completed till exercise 2.11 and added vanilla 'data for countries' react project

--- Exercises/phonebook/src/App.js
+++ Exercises/phonebook/src/App.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/header';
 import Persons from './components/persons';
 import Filter from './components/filter';
 import PersonForm from './components/personform';
+import axios from 'axios';
 
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ showAll, setShowAll] = useState(true)
   const [ searchText, setSearchText] = useState('')
+
+  useEffect(()=> {
+    axios.get('http://localhost:3001/persons')
+      .then(response=> {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
